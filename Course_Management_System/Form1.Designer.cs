3c6e1bd41eb378892ced53edf94271ea0351c6c6
Abuuu257 <abdulrahman25729@gmail.com>
2025-01-20 10:23:39 +0530

    first commit

---
+++ Course_Management_System/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Course_Management_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            comboBox1 = new ComboBox();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(40, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(171, 62);
+            label1.TabIndex = 0;
+            label1.Text = "LOGIN";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold);
+            label2.Location = new Point(265, 154);
+            label2.Name = "label2";
+            label2.Size = new Size(121, 31);
+            label2.TabIndex = 0;
+            label2.Text = "Username";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold);
+            label3.Location = new Point(265, 258);
+            label3.Name = "label3";
+            label3.Size = new Size(114, 31);
+            label3.TabIndex = 0;
+            label3.Text = "Password";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(265, 292);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(302, 27);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(337, 343);
+            button1.Name = "button1";
+            button1.Size = new Size(155, 40);
+            button1.TabIndex = 2;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Instructor", "Student" });
+            comboBox1.Location = new Point(463, 154);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(104, 28);
+            comboBox1.TabIndex = 3;
+            comboBox1.Text = "Select User";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(265, 188);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(302, 27);
+            textBox2.TabIndex = 1;
+            textBox2.TextChanged += textBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(864, 500);
+            Controls.Add(comboBox1);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private Button button1;
+        private ComboBox comboBox1;
+        private TextBox textBox2;
+    }
+}
