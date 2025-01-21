85106ce7f139c3d3b31d4a854573dfce4a76f7b9
Abuuu257 <abdulrahman25729@gmail.com>
2025-01-21 12:41:13 +0530

    10th commit

--- Course_Management_System/StdLoginForm.Designer.cs
+++ Course_Management_System/StdLoginForm.Designer.cs
@@ -41,7 +41,7 @@
             label2.Anchor = AnchorStyles.None;
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(213, 85);
+            label2.Location = new Point(218, 11);
             label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
             label2.Size = new Size(384, 37);
@@ -53,7 +53,7 @@
             // 
             button1.Anchor = AnchorStyles.None;
             button1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
-            button1.Location = new Point(279, 149);
+            button1.Location = new Point(286, 121);
             button1.Margin = new Padding(2);
             button1.Name = "button1";
             button1.Size = new Size(231, 44);
@@ -66,7 +66,7 @@
             // 
             button2.Anchor = AnchorStyles.None;
             button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
-            button2.Location = new Point(633, 74);
+            button2.Location = new Point(720, 11);
             button2.Margin = new Padding(2);
             button2.Name = "button2";
             button2.Size = new Size(69, 35);
@@ -79,7 +79,7 @@
             // 
             button3.Anchor = AnchorStyles.None;
             button3.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic);
-            button3.Location = new Point(279, 258);
+            button3.Location = new Point(286, 230);
             button3.Margin = new Padding(2);
             button3.Name = "button3";
             button3.Size = new Size(231, 38);
@@ -92,7 +92,7 @@
             // 
             button4.Anchor = AnchorStyles.None;
             button4.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic);
-            button4.Location = new Point(268, 206);
+            button4.Location = new Point(275, 178);
             button4.Margin = new Padding(2);
             button4.Name = "button4";
             button4.Size = new Size(254, 38);
@@ -105,7 +105,7 @@
             // 
             button5.Anchor = AnchorStyles.None;
             button5.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic);
-            button5.Location = new Point(279, 313);
+            button5.Location = new Point(286, 285);
             button5.Margin = new Padding(2);
             button5.Name = "button5";
             button5.Size = new Size(231, 38);
